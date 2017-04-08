f903ae99d96561b3a7b268902270aae5479dafe5
Jon Oropeza <jon_oropeza@homedepot.com>
2017-04-08 11:30:07 -0700

    kill all the redux crap

--- src/your-code-goes-here/routes.js
+++ src/your-code-goes-here/routes.js
@@ -2,13 +2,7 @@ import React from 'react';
 import { Route } from 'react-router';
 
 import AppRoot from './containers/AppRoot';
-import Cities from './containers/cities';
-import City from './containers/city';
-
 
 export default (
-    <Route path="/" component={AppRoot}>
-        <Route path="cities" component={Cities} />
-        <Route path="city/:id" component={City} />
-    </Route>
+    <Route path="/" component={AppRoot} />
 );
